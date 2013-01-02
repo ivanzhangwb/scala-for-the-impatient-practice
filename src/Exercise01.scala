49ec9cf674f51025f5dee9ba524c7c0676e1bdce
ivanzhangwb <zhangwenbo@ivanzhangwb.local>
2013-01-02 14:32:07 +0800

    add source code for exercise charpter 1

--- src/Exercise01.scala
+++ src/Exercise01.scala
@@ -69,5 +69,64 @@ class Exercise01 {
     stringBuilder.toString
   }
   
+  /**
+   * (9) : 获取字符串的第一个和最后一个字符的方法有： ->_<-
+   * scala> "ivanzhang".head
+   * res20: Char = i
+   * scala> "ivanzhang".last
+   * res21: Char = g
+   * scala> "ivanzhang".first
+   * warning: there were 1 deprecation warnings; re-run with -deprecation for details
+   * res22: Char = i
+   * scala> "ivanzhang".indexOf(1)
+   * res23: Int = -1
+   * scala> "ivanzhang".substring(0,1)
+   * res24: java.lang.String = i
+   * scala> "ivanzhang".substring("ivanzhang".length()-1)
+   * res25: java.lang.String = g
+   */
   
+  /**
+   * (10): 说说take,drop,takeRight 和 dropRight 这些字符串函数的作用：
+   * 
+   * take: 获取最开始的几个元素 (从左边开始)
+   * 	scala> "ivanzhang".take(3)
+		res26: String = iva
+		
+   * drop: 获取除了指定的元素外的其他元素:（从左边开始）
+   * 	"ivanzhang".drop(3)
+		res29: String = nzhang
+		
+   * takeRight: 从右边开始
+   * 	scala> "ivanzhang".takeRight(3)
+		res31: String = ang
+		
+   * dropRight: 从右边开始
+   * 	scala> "ivanzhang".dropRight(3)
+		res32: String = ivanzh
+   * 
+   * 它们与substring的差别:
+   * 1. substring如果获取的元素个数超过字符串长度，会报错：
+   * 	scala> "ivanzhang".take(13)
+		res27: String = ivanzhang
+		
+		scala> "ivanzhang".substring(0,13)
+		java.lang.StringIndexOutOfBoundsException: String index out of range: 13
+			at java.lang.String.substring(String.java:1934)
+			at .<init>(<console>:14)
+			at .<clinit>(<console>)
+			at .<init>(<console>:11)
+			at .<clinit>(<console>)
+			at $print(<console>)
+			at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+			at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:39)
+			at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:25)
+			at java.lang.reflect.Method.invoke(Method.java:597)
+			at scala.tools.nsc.interpreter.IMain$ReadEvalPrint.call(IMain.scala:704)
+			at scala.tools.nsc.interpreter.IMain$Request$$anonfun$14.apply(IMain.scala:920)
+			at scala.tools.nsc.interpreter.Line$$anonfun$1.apply$mcV$sp(Line.scala:43)
+			at scala.tools.nsc.io.package$$anon$2.run(package.scala:25)
+			at java.lang.Thread.run(Thread.java:680)
+	*
+   */
 }
